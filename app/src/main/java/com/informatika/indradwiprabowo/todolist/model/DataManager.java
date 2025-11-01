005345f90879c087561da106446311d2e9b992d8
kaiharvest <indradwiprabowo27@gmail.com>
2025-11-01 18:51:43 +0700

    commit

---
+++ app/src/main/java/com/informatika/indradwiprabowo/todolist/model/DataManager.java
@@ -0,0 +1,7 @@
+package com.informatika.indradwiprabowo.todolist.model;
+
+import java.util.ArrayList;
+
+public class DataManager {
+    public static ArrayList<Task> taskList = new ArrayList<>();
+}
---
+++ app/src/main/java/com/informatika/indradwiprabowo/todolist/model/Task.java
@@ -0,0 +1,11 @@
+package com.informatika.indradwiprabowo.todolist.model;
+
+public class Task {
+    public String title;
+    public String deadline; // format: "10/05/2025"
+
+    public Task(String title, String deadline) {
+        this.title = title;
+        this.deadline = deadline;
+    }
+}
